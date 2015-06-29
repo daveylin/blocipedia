aaa63184b8d279fae3d27912c8747d898ec6cb11
David Lin <daveylin@gmail.com>
2015-06-29 06:37:58 +0000

    Wiki CRUD completed

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,13 +1,53 @@
 class WikisController < ApplicationController
   def index
+    @wikis = Wiki.all
   end
 
   def show
+    @wiki = Wiki.find(params[:id])
   end
 
   def new
+    @wiki = Wiki.new
   end
 
+  def create
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body, :private))
+    @user = current_user
+    @wiki.user_id = @user.id
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end
+  
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+  
+   def update
+     @wiki = Wiki.find(params[:id])
+     if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+       flash[:notice] = "Wiki was updated."
+       redirect_to @wiki
+     else
+       flash[:error] = "There was an error saving the wiki. Please try again."
+       render :edit
+     end
+   end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+ 
+    if @wiki.destroy
+      flash[:notice] = "Item was deleted successfully."
+      redirect_to [@wiki], notice: "Item was deleted successfully."
+     else
+      flash[:error] = "There was an error deleting the item. Please try again."
+     end
+    
   end
 end
